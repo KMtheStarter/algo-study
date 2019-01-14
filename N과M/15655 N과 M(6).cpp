93252a3eec107bc014368f1d55df4cad6e226c67
KMtheStarter <39829629+KMtheStarter@users.noreply.github.com>
2019-01-14 19:34:54 +0900

    Create 15655 N과 M(6).cpp

---
+++ N과M/15655 N과 M(6).cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+vector<int> seq;
+vector<int> list;
+vector<bool> visit;
+// 백트래킹 기법
+void func() {
+    int size = seq.size();
+    if (size == M) {
+        for (int i = 0; i < M - 1; i++){
+            if(seq[i] > seq[i + 1])
+                return;
+        }
+        for (int i = 0; i < M; i++) {
+            cout << seq[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if(!visit[i]){
+            visit[i] = true;
+            seq.push_back(list[i - 1]);
+            func();
+            visit[i] = false;
+            seq.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    visit = vector<bool>(N + 1, false);
+    cin >> N >> M;
+    for (int i = 0; i < N; i++){
+        int n;
+        cin >> n;
+        list.push_back(n);
+    }
+    sort(list.begin(), list.end());
+    func();
+    return 0;
+}
