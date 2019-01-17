b149c49d217af0d92480b68e30b2ef16b8c2a7c4
KMtheStarter <39829629+KMtheStarter@users.noreply.github.com>
2019-01-17 20:02:02 +0900

    Update 16194 카드 구매하기-2.cpp

--- DP/16194 카드 구매하기-2.cpp
+++ DP/16194 카드 구매하기-2.cpp
@@ -9,6 +9,7 @@ int main(){
     vector<int> list(n+1);
     for (int i = 1; i <= n; i++)
         cin >> list[i];
+    // dp를 -1로 초기화하고 조건문에서 dp[i] == -1 || dp[i] > dp[i - j] + list[j] 하는 방법도 
     vector<int> dp(n+1, INT16_MAX);
     dp[0] = 0;
     for (int i = 1; i <= n; i++){
